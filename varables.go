6a95aed5c827a3a3c78424a6cb394bf925bb43d9
misterF <myspace773@outlook.com>
2026-06-14 21:08:09 -0600

    using variables

---
+++ varables.go
@@ -0,0 +1,21 @@
+//this is how you declare variables
+
+package main
+
+import "fmt"
+
+func main(){
+	//short declaration
+	message:= "hello, world" //type inferred as string
+	age:= 26	//type inferred as int
+	score:= 94.5    //type inferred as float64
+
+	//with the keyword var
+	//with the type and no initial value
+	var count int //initialized to 0
+	//with a type and initial value
+	var name string= "GuateGolang"
+	fmt.Println(count, name, message,age,score)
+}
+
+	
