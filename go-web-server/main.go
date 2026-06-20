b9f07b2e467318694c6d08860e312e119052fbcc
misterF <myspace773@outlook.com>
2026-06-20 13:32:13 -0600

    adding a web server

---
+++ go-web-server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+	sr "go-web-server/handlers"
+	"time"
+)
+func main(){
+	//1.initialize a custom request router(multiplexer)
+	mux:=http.NewServeMux()
+
+	//2. register route paths and their corresponding handler functions
+	mux.HandleFunc("/",sr.HomeHandler)
+	mux.HandleFunc("/api/data",sr.ApiHandler)
+	//3. configure explicit server parameters for safety and timeouts
+	server:= &http.Server{
+		Addr: ":8080",  // listen on port 8080
+		Handler:  mux, //inject our router custom configuration
+		ReadTimeout: 10 * time.Second, //// Max time reading request headers/body
+		WriteTimeout: 10 * time.Second, //Max time writing response
+		IdleTimeout: 30 * time.Second, // Max time to keep connection open idle 
+	}
+	//4. Fire up the server
+	fmt.Println("Server successfully setarted on http://localhost:8080")
+	if err:= server.ListenAndServe();err!= nil && err!= http.ErrServerClosed{
+		log.Fatalf("Server failed to start: %v",err)
+	}
+}
