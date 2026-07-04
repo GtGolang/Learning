3b70b8ee203660ed02c6c11dce9bb6e1441e4e05
misterF <myspace773@outlook.com>
2026-07-04 16:47:33 -0600

    adding css style and new view

--- go-web-server/handlers/handlers.go
+++ go-web-server/handlers/handlers.go
@@ -1,47 +1,54 @@
 package handlers
-import(
-	"net/http"
+
+import (
 	"fmt"
+	"net/http"
 	"path/filepath"
 )
+
 // HomeHandler handles request sent to the root "/" path
-func HomeHandler(w http.ResponseWriter, r *http.Request){
-	 //ensure the path is exactly "/" to avoid matching all sub -paths
-	 if r.URL.Path!="/"{
-		http.NotFound(w,r)
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	//ensure the path is exactly "/" to avoid matching all sub -paths
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
 		return
-	 }
-	 //fmt.Fprint(w,"Welcome to the fundation of something Bg")
-	 http.ServeFile(w,r,"./static/index.html")
+	}
+	//fmt.Fprint(w,"Welcome to the fundation of something Bg")
+	http.ServeFile(w, r, "./static/index.html")
 	fmt.Println("running the page index.html")
 }
 
-
-func ApiHandler(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("content-Type","aplication/json")
+func ApiHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "aplication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message": "hello from the Api endpoint!"}`))
 }
-func CssHandler(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path!="/static/style.css"{
-		http.NotFound(w,r)
-		fmt.Println("ruta no encontrada") 
+func CssHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/static/style.css" {
+		http.NotFound(w, r)
+		fmt.Println("ruta no encontrada")
 		return
 	}
 	fmt.Println("ruta encontrada /static/style.css")
 
 	// 1. Clean the path to prevent security issues (directory traversal)
 	// r.URL.Path will be something like "/static/style.css"
-// filepath.Clean() does change "/static/style.css" into "\static\style.css" 
+	// filepath.Clean() does change "/static/style.css" into "\static\style.css"
 	cleanedPath := filepath.Clean(r.URL.Path)
 
-	// 2. Join it with your local folder name. 
+	// 2. Join it with your local folder name.
 	// This turns "\static\style.css" into ".\static\style.css"
 	filePath := "." + cleanedPath
 
-        // 3. Let Go safely stream the file to the browser
+	// 3. Let Go safely stream the file to the browser
 	http.ServeFile(w, r, filePath)
-		//http.ServeFile(w,r,"./static/style.css")
+	//http.ServeFile(w,r,"./static/style.css")
 	fmt.Println("css served")
 }
 
+func ServeImages(w http.ResponseWriter, r *http.Request) {
+	cleanedPath := filepath.Clean(r.URL.Path)
+	filePath := "." + cleanedPath
+	http.ServeFile(w, r, filePath)
+	fmt.Println("img served")
+}
--- go-web-server/main.go
+++ go-web-server/main.go
@@ -1,31 +1,33 @@
 package main
 
-import(
+import (
 	"fmt"
+	sr "go-web-server/handlers"
 	"log"
 	"net/http"
-	sr "go-web-server/handlers"
 	"time"
 )
-func main(){
+
+func main() {
 	//1.initialize a custom request router(multiplexer)
-	mux:=http.NewServeMux()
+	mux := http.NewServeMux()
 
 	//2. register route paths and their corresponding handler functions
-	mux.HandleFunc("/",sr.HomeHandler)
-	mux.HandleFunc("/static/",sr.CssHandler)
-	mux.HandleFunc("/api/data",sr.ApiHandler)
+	mux.HandleFunc("/", sr.HomeHandler)
+	mux.HandleFunc("/static/", sr.CssHandler)
+	mux.HandleFunc("/api/data", sr.ApiHandler)
+	mux.HandleFunc("/img/", sr.ServeImages)
 	//3. configure explicit server parameters for safety and timeouts
-	server:= &http.Server{
-		Addr: ":8080",  // listen on port 8080
-		Handler:  mux, //inject our router custom configuration
-		ReadTimeout: 10 * time.Second, //// Max time reading request headers/body
+	server := &http.Server{
+		Addr:         ":8080",          // listen on port 8080
+		Handler:      mux,              //inject our router custom configuration
+		ReadTimeout:  10 * time.Second, //// Max time reading request headers/body
 		WriteTimeout: 10 * time.Second, //Max time writing response
-		IdleTimeout: 30 * time.Second, // Max time to keep connection open idle 
+		IdleTimeout:  30 * time.Second, // Max time to keep connection open idle
 	}
 	//4. Fire up the server
 	fmt.Println("Server successfully setarted on http://localhost:8080")
-	if err:= server.ListenAndServe();err!= nil && err!= http.ErrServerClosed{
-		log.Fatalf("Server failed to start: %v",err)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Server failed to start: %v", err)
 	}
 }
